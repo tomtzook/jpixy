03ecb7b2e8a6275daeee35ce795b59997e679be5
Tom Tzook <tomtzook@gmail.com>
2020-02-07 14:20:51 +0200

    created different subproject

---
+++ pixyusb/build/cmake/CMakeFiles/pixyjni.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tomtzook/projects/jpixy/pixyusb/src/main/jni/pixyjni.cpp" "/home/tomtzook/projects/jpixy/pixyusb/build/cmake/CMakeFiles/pixyjni.dir/main/jni/pixyjni.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/tomtzook/projects/jpixy/pixyusb/src/../libpixy"
+  "/usr/include/libusb-1.0"
+  "/usr/lib/jvm/java-8-oracle/include"
+  "/usr/lib/jvm/java-8-oracle/include/linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
